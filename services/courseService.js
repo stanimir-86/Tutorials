cd5016af06b5d9f9d60eb2fab0746fdd05fada30
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-04-08 23:46:12 +0300

    Update courseService.js

--- services/courseService.js
+++ services/courseService.js
@@ -5,6 +5,12 @@ async function getAllByDate() {
     return Course.find({}).sort({ createdAt: 1 }).lean();
 }
 
+async function getRecent() {
+
+    return Course.find({}).sort({ userCount: -1 }).limit(3).lean();
+
+}
+
 async function createCourse(course) {
     return Course.create(course);
 }
@@ -12,5 +18,5 @@ async function createCourse(course) {
 module.exports = {
     getAllByDate,
     createCourse,
-
+    getRecent,
 }
