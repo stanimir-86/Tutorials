14bbf4fab9f6c2325a12b3055616c45950e0f884
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-04-08 23:02:04 +0300

    Update Course.js

--- models/Course.js
+++ models/Course.js
@@ -4,20 +4,19 @@ const URL_PATTERN = /https?:\/\/./i;
 const courseSchema = new Schema(({
     title: { type: String, required: true, minlength: [4, 'Course title must be at least 4 charachters long'] },
     description: {
-        type: String, required: true,
+        type: String,
         minlength: [20, 'Course description must be at least 20 charachters long'],
         maxlength: [50, 'Course description must be maximum 50 charachters long']
     },
     imageUrl: {
         type: String,
-        required: true,
         validate: {
             validator: (value) => URL_PATTERN.test(value),
             message: 'Invalid URL'
         }
     },
-    duration: { type: String, required: true },
-    createAt: { type: String, required: true },
+    duration: { type: String, required: [true,'Duration is required'] },
+    createdAt: { type: String, required: true, default: () => (new Date()).toISOString().slice(0, 10) },
     users: { type: [Types.ObjectId], ref: 'User', default: [] },
     owner: { type: Types.ObjectId, ref: 'User' },
 }));
@@ -31,4 +30,4 @@ courseSchema.index({ title: 1 }, {
 
 const Course = model('Course', courseSchema);
 
-model.exports = Course;
+module.exports = Course;
