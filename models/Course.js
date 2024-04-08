8978dbdf29a5d8e431e03dc6bf4d005ad387d121
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-04-08 23:46:10 +0300

    Update Course.js

--- models/Course.js
+++ models/Course.js
@@ -15,9 +15,10 @@ const courseSchema = new Schema(({
             message: 'Invalid URL'
         }
     },
-    duration: { type: String, required: [true,'Duration is required'] },
+    duration: { type: String, required: [true, 'Duration is required'] },
     createdAt: { type: String, required: true, default: () => (new Date()).toISOString().slice(0, 10) },
     users: { type: [Types.ObjectId], ref: 'User', default: [] },
+    userCount: { type: Number, default: 0 },
     owner: { type: Types.ObjectId, ref: 'User' },
 }));
 
