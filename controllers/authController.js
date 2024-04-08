9d38fa15e6c0f78915abb65244e0136526fb99e4
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-04-08 22:12:26 +0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const authController = require('express').Router();
+const { validationResult, body } = require('express-validator');
 const { register, login } = require('../services/userService.js');
 const { parseError } = require('../util/parser.js');
 
@@ -12,35 +13,49 @@ authController.get('/register', (req, res) => {
     });
 });
 
-authController.post('/register', async (req, res) => {
-    try {
-        if (req.body.username == '' || req.body.password == '') {
-            throw new Error('All fields are required')
-        }
-        if (req.body.password != req.body.repass) {
-            throw new Error('Passwords dont match')
-        }
-        const token = await register(req.body.username, req.body.password);
-
-
-        res.cookie('token', token);
-        res.redirect('/');
-
-    } catch (error) {
-        console.log(error);
-        const errors = parseError(error);
-
-
-        res.render('register', {
-            title: 'Register page',
-            errors,
-            body: {
-                username: req.body.username
+authController.post('/register',
+    body('username')
+        .isLength({ min: 5 }).withMessage('Username must be atleast 5 charachters long')
+        .isAlphanumeric().withMessage('Username may contain only letters and numbers'),
+    body('password')
+        .isLength({ min: 5 }).withMessage('Password must be atleast 5 charachters long')
+        .isAlphanumeric().withMessage('Username may contain only letters and numbers'),
+    async (req, res) => {
+        try {
+            const { errors } = validationResult(req);
+            if (errors.length > 0) {
+                throw errors
             }
-        });
+            // if (req.body.username == '' || req.body.password == '') {
+            //     throw new Error('All fields are required')
+            // }
+            // if (req.body.password.length < 5) {
+            //     throw new Error('Password must be atleast 5 charachters long');
+            // }
+            if (req.body.password != req.body.repass) {
+                throw new Error('Passwords dont match')
+            }
+            const token = await register(req.body.username, req.body.password);
 
-    }
-});
+
+            res.cookie('token', token);
+            res.redirect('/');
+
+        } catch (error) {
+            console.log(error);
+            const errors = parseError(error);
+
+
+            res.render('register', {
+                title: 'Register page',
+                errors,
+                body: {
+                    username: req.body.username
+                }
+            });
+
+        }
+    });
 
 authController.get('/login', (req, res) => {
     res.render('login', {
@@ -52,7 +67,7 @@ authController.post('/login', async (req, res) => {
         const token = await login(req.body.username, req.body.password);
 
         res.cookie('token', token);
-        res.redirect('/')//TODO replace with redirect by assigment
+        res.redirect('/')
     } catch (error) {
         const errors = parseError(error);
         res.render('login', {
