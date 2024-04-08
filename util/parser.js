7df68d213f8c8d81021d26785140b0d0d3619237
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-04-08 22:12:46 +0300

    Update parser.js

--- util/parser.js
+++ util/parser.js
@@ -1,7 +1,10 @@
 function parseError(error) {
     if (error.name == "ValidationError") {
         return Object.values(error.errors).map(v => v.message)
+    } else if (Array.isArray(error)) {
+        return error.map(x => x.msg)
     } else {
+
 
         return error.message.split('\n');
     }
