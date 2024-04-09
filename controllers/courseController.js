5692486f56508d3e9446ce854debf4873115a5d0
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-04-09 13:12:13 +0300

    Update courseController.js

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,4 +1,4 @@
-const { createCourse, getById } = require('../services/courseService.js');
+const { createCourse, getById, deleteById, updateById } = require('../services/courseService.js');
 const { parseError } = require('../util/parser.js');
 
 const courseController = require('express').Router();
@@ -12,12 +12,22 @@ courseController.get('/create', (req, res) => {
 courseController.get('/:id', async (req, res) => {
     const course = await getById(req.params.id);
 
-    course.isOwner = course.owner.toString() == req.user._id.toString();
+    // course.isOwner = course.owner.toString() == req.user._id.toString();
 
     res.render('details', {
         title: course.title,
         course
     });
+});
+
+courseController.get('/:id/delete', async (req, res) => {
+    const course = await getById(req.params.id);
+    if (course.owner.toString() != req.user._id.toString()) {
+        return res.redirect('/auth/login');
+    }
+
+    await deleteById(req.params.id);
+    res.redirect('/');
 });
 
 courseController.post('/create', async (req, res) => {
@@ -42,4 +52,32 @@ courseController.post('/create', async (req, res) => {
 
 });
 
+courseController.get('/:id/edit', async (req, res) => {
+    const course = await getById(req.params.id);
+    if (course.owner.toString() != req.user._id.toString()) {
+        return res.redirect('/auth/login');
+    }
+    res.render('edit', {
+        title: 'Edit Course',
+        course
+    });
+});
+
+courseController.post('/:id/edit', async (req, res) => {
+    const course = await getById(req.params.id);
+    if (course.owner.toString() != req.user._id.toString()) {
+        return res.redirect('/auth/login');
+    }
+    try {
+        await updateById(req.params.id, req.body);
+        
+        res.redirect(`/course/${req.params.id}`);
+    } catch (err) {
+        res.render('edit', {
+            title: 'Edit Course',
+            errors: parseError(err),
+            course: req.body,
+        });
+    }
+});
 module.exports = courseController;
